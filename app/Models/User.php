d0de04c508fe000ce22f00ab42cf4411d358622f
Alex López <alexdracma@gmail.com>
2023-05-07 23:14:49 +0200

    Added relationships to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -62,29 +65,32 @@ class User extends Authenticatable
         'profile_photo_url',
     ];
 
-    //functions
-    public function fullName() {
-        return $this->name . ' ' . $this->surname;
-    }
-
     //relationships
-    public function shoppingLists() {
+    public function shoppingLists(): HasMany {
         return $this->hasMany(ShoppingList::class);
     }
 
-    public function posts() {
+    public function posts(): HasMany {
         return $this->hasMany(Post::class);
     }
 
-    public function favorites() {
+    public function favorites(): BelongsToMany {
         return $this->belongsToMany(Recipe::class, 'favorites');
     }
 
-    public function pantries() {
+    public function pantries(): BelongsToMany {
         return $this->belongsToMany(Ingredient::class, 'pantries');
     }
 
-    public function weeklyPlans() {
+    public function weeklyPlans(): BelongsToMany {
         return $this->belongsToMany(Recipe::class, 'weekly_plans');
+    }
+
+    public function likesFromPosts(): HasManyThrough {
+        return $this->hasManyThrough(Like::class, Post::class);
+    }
+
+    public function commentsFromPosts(): HasManyThrough {
+        return $this->hasManyThrough(Comment::class, Post::class);
     }
 }
