b0d6e1eca4a4c99fada62b8e8388fef99b8eb68c
Alex López <alexdracma@gmail.com>
2023-05-14 14:54:42 +0200

    Mostly completed favorites controller

--- app/Http/Livewire/Favorites.php
+++ app/Http/Livewire/Favorites.php
@@ -3,11 +3,56 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Usernotnull\Toast\Concerns\WireToast;
 
 class Favorites extends Component
 {
+    use WireToast;
+    public $userFavorites;
+    public $shownFavorites;
+    public $query = '';
+
+    //livewire lifecycle
+    public function updatedQuery() {
+        $this->updateFavorites();
+    }
+
+    public function mount() {
+        $this->fillArrays();
+    }
+
     public function render()
     {
         return view('livewire.favorites');
+    }
+
+    //My functions
+    private function updateFavorites() {
+        $temp = [];
+        foreach ($this->userFavorites as $recipe) {
+            if (str_contains(strtoupper($recipe->title),strtoupper($this->query))) {
+                array_push($temp, $recipe);
+            }
+        }
+        $this->shownFavorites = $temp;
+    }
+
+    private function fillArrays() {
+        $this->userFavorites = Auth::user()->favorites;
+        $this->shownFavorites = clone $this->userFavorites;
+    }
+
+    public function removeFavorite($recipeId) { //removes the favorite from the user's list if it exists
+
+        if (Auth::user()->favorites()->where('recipe_id', $recipeId)->exists()) {
+
+            Auth::user()->favorites()->detach($recipeId);
+            $this->fillArrays();
+            toast()
+                ->success('The recipe has been successfully removed from your favorites')
+                ->push();
+        }
+
     }
 }
--- resources/views/livewire/favorites.blade.php
+++ resources/views/livewire/favorites.blade.php
@@ -6,287 +6,38 @@
     <div class="row m-auto searchContainer py-5 d-flex justify-content-center">
         <div class="col-12 position-relative p-0">
             <img src="assets/icons/search.svg" class="searchIcon">
-            <input type="search" name="" placeholder="Search your favorite recipes">
+            <input type="search" wire:model="query" placeholder="Search your favorite recipes">
         </div>
     </div>
 
     <!-- Recipes -->
     <div class="row m-auto">
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe1.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Simple Chicken Salad</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>1</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
+        @foreach($shownFavorites as $favorite)
+            <div class="card mb-3 ps-0">
+                <div class="row h-100">
+                    <div class="col-lg-4 pe-0">
+                        <img src="assets/images/Recipe1.png" class="recipeImg" alt="...">
+                    </div>
+                    <div class="col-lg-8 p-0">
+                        <div class="card-body h-100 d-flex flex-column justify-content-end">
+                            <h4 class="card-title mb-auto">{{ $favorite->title }}</h4>
+                            <div class="card-text mt-2 d-flex">
+                                <img src="assets/icons/portions.svg" class="recipeIcon">
+                                <span class="ms-2 align-self-end"><b>{{ $favorite->servings }}</b> Serving</span>
+                            </div>
+                            <div class="card-text d-flex justify-content-between align-items-end">
+                            <span class="d-flex">
                                 <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>12</b> Minutes</span>
+                                <span class="ms-2 align-self-end"><b>{{ $favorite->readyInMinutes }}</b> Minutes</span>
                             </span>
-                            <img src="assets/icons/like.svg" class="recipeIcon">
+                                <img src="assets/icons/like_full.svg" class="recipeIcon cursor-pointer"
+                                    wire:click="removeFavorite({{ $favorite->id }})">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe2.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Brined Chicken Breast with Sautéed Onion Dipping Sauce</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>4</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>25</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like_full.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe3.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Stuffed Chicken Breast</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>2</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>45</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like_full.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe4.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Stir Fried Quinoa, Brown Rice and Chicken Breast</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>3</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>120</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe1.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Simple Chicken Salad</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>1</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>12</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe2.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Brined Chicken Breast with Sautéed Onion Dipping Sauce</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>4</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>25</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like_full.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe3.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Stuffed Chicken Breast</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>2</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>45</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like_full.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe4.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Stir Fried Quinoa, Brown Rice and Chicken Breast</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>3</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>120</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe1.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Simple Chicken Salad</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>1</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>12</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe2.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Brined Chicken Breast with Sautéed Onion Dipping Sauce</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>4</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>25</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like_full.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe3.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Stuffed Chicken Breast</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>2</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>45</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like_full.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card mb-3 ps-0">
-            <div class="row h-100">
-                <div class="col-lg-4 pe-0">
-                    <img src="assets/images/Recipe4.png" class="recipeImg" alt="...">
-                </div>
-                <div class="col-lg-8 p-0">
-                    <div class="card-body h-100 d-flex flex-column justify-content-end">
-                        <h4 class="card-title mb-auto">Stir Fried Quinoa, Brown Rice and Chicken Breast</h4>
-                        <div class="card-text mt-2">
-                            <img src="assets/icons/portions.svg" class="recipeIcon">
-                            <span class="ms-2"><b>3</b> Serving</span>
-                        </div>
-                        <div class="card-text d-flex justify-content-between align-items-end">
-                            <span>
-                                <img src="assets/icons/time.svg" class="recipeIcon">
-                                <span class="ms-2"><b>120</b> Minutes</span>
-                            </span>
-                            <img src="assets/icons/like.svg" class="recipeIcon">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    @endforeach
     </div>
 </div>
+{{--$favorite->id--}}
