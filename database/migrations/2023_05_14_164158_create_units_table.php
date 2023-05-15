5385699b77f8e5537173841c2f4d0599c95fa20e
Alex López <alexdracma@gmail.com>
2023-05-15 10:59:35 +0200

    Added unique constraint to units table

--- database/migrations/2023_05_14_164158_create_units_table.php
+++ database/migrations/2023_05_14_164158_create_units_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('units', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->nullable(false);
+            $table->string('name')->nullable(false)->unique();
             $table->timestamps();
         });
     }
