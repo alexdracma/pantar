a0a4084dda7af2c857678714a3c3bd5cab8899ae
Alex López <alexdracma@gmail.com>
2023-05-29 23:59:46 +0200

    Fixed modal

--- resources/views/components/dialog-modal.blade.php
+++ resources/views/components/dialog-modal.blade.php
@@ -1,17 +1,20 @@
 @props(['id' => null, 'maxWidth' => null])
 
 <x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    <div class="px-6 py-4">
-        <div class="text-lg font-medium text-gray-900">
-            {{ $title }}
+    <div class="mb-6 bg-background3 rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full sm:max-w-2xl sm:mx-auto border-2 border-black">
+        <div class="px-6 py-4">
+            <div class="text-lg font-medium text-gray-900">
+                {{ $title }}
+            </div>
+
+            <div class="mt-4 text-sm text-gray-600">
+                {{ $content }}
+            </div>
         </div>
 
-        <div class="mt-4 text-sm text-gray-600">
-            {{ $content }}
+        <div class="flex flex-row justify-end px-6 py-4 text-right">
+            {{ $footer }}
         </div>
     </div>
 
-    <div class="flex flex-row justify-end px-6 py-4 text-right">
-        {{ $footer }}
-    </div>
 </x-modal>
