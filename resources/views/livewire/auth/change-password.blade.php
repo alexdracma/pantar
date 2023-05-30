82366b5624219c52cdf2112e41b8d5fbcb88c94f
Alex López <alexdracma@gmail.com>
2023-05-30 10:21:53 +0200

    Fixed change password modal

--- resources/views/livewire/auth/change-password.blade.php
+++ resources/views/livewire/auth/change-password.blade.php
@@ -11,20 +11,20 @@
         <x-slot name="content">
             <form wire:submit.prevent="updatePassword">
                 <div class="col-span-6 sm:col-span-4 mt-3">
-                    <x-label for="current_password" value="{{ __('Current Password') }}" />
-                    <x-input id="current_password" type="password" class="mt-1 block w-full" wire:model.defer="state.current_password" autocomplete="current-password" />
+                    <label for="password">Current Password</label>
+                    <x-input id="current_password" type="password" class="mt-1 block w-full border-2 border-black" wire:model.defer="state.current_password" autocomplete="current-password" />
                     <x-input-error for="current_password" class="mt-2 text-danger" />
                 </div>
 
                 <div class="col-span-6 sm:col-span-4 mt-3">
-                    <x-label for="password" value="{{ __('New Password') }}" />
-                    <x-input id="password" type="password" class="mt-1 block w-full" wire:model.defer="state.password" autocomplete="new-password" />
+                    <label for="password">New Password</label>
+                    <x-input id="password" type="password" class="mt-1 block w-full border-2 border-black" wire:model.defer="state.password" autocomplete="new-password" />
                     <x-input-error for="password" class="mt-2 text-danger" />
                 </div>
 
                 <div class="col-span-6 sm:col-span-4 mt-3">
-                    <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                    <x-input id="password_confirmation" type="password" class="mt-1 block w-full" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
+                    <label for="password">Confirm Password</label>
+                    <x-input id="password_confirmation" type="password" class="mt-1 block w-full border-2 border-black" wire:model.defer="state.password_confirmation" autocomplete="new-password" />
                     <x-input-error for="password_confirmation" class="mt-2 text-danger" />
 
                 </div>
